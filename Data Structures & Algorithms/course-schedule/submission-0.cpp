5b2108172075d5278399d1bfb01f67b83523f1b0
Siddhant <siddhantmpai@gmail.com>
2026-08-16 18:25:08 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses, 0);
+        vector<int> visited(numCourses, 0);
+
+        for(auto a: prerequisites){
+            adj[a[1]].push_back(a[0]);
+            indeg[a[0]]++;
+        }
+
+        queue<int> q;
+        vector<int> res;
+        for(int i = 0; i < numCourses; i++){
+            if(!indeg[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+
+        while(!q.empty()){
+            int idx = q.front();
+            q.pop();
+            res.push_back(idx);
+
+            for(int i: adj[idx]){
+                indeg[i]--;
+                if(!indeg[i] && !visited[i]){
+                    q.push(i);
+                    visited[i] = 1;
+                }
+            }
+        }
+
+        if(res.size() == numCourses) return true;
+        else return false;
+    }
+};
